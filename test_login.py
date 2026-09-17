44d64859c2b2eac59ad3bcd4249e7b2db46c212f
281751106@qq.com <LeoDong@yingfu.com>
2026-09-17 11:22:53 +0800

    D2 修改代

--- test_login.py
+++ test_login.py
@@ -1,26 +1,62 @@
 import requests
+from numpy.f2py.crackfortran import usepattern
 
 BASE_URL = "https://httpbin.org"
+#BASE_URL = "https://baidu.com"
 
-def test_login():
+
+'''Login API 测试'''
+# def test_login():
+#     url = f"{BASE_URL}/post"
+#     payload = {"username": "admin", "password": "123456"}
+#     r = requests.post(url, json=payload)
+#     print("状态码", r.status_code)
+#     print("返回", r.json())
+#
+#     assert r.status_code == 200
+#     assert r.json()["json"]["username"] == "admin"
+
+# '''GET API 测试'''
+# def test_with_header():
+#     url = f"{BASE_URL}/get"
+#     hearders ={"User-Agent": "pytest-demo", "Authorization": "Bearer fake"}
+#
+#     r = requests.request(method = "GET", url = url, headers = hearders)
+#
+#     print("GET 状态码", r.status_code)
+#     print("Json打印", r.json())
+#
+#     assert r.status_code == 200
+#     assert r.json()["headers"]["Authorization"] == "Bearer fake"
+#
+# '''Token API 测试'''
+# def test_with_protected_api():
+#     token = "fake-token-123"
+#     headers = {"Authorization": f"Bearer"}
+#
+#     #r.requests(method = "GET", url = f"{BASE_URL}/get", headers = headers)
+#     r =  requests.get(url = f"{BASE_URL}/get", headers = headers)
+#     assert r.status_code == 200
+#     assert r.json()["headers"]["Authorization"] == f"Bearer {token}"
+
+
+'''Real API 测试'''
+def test_real_api():
+    #Login
     url = f"{BASE_URL}/post"
     payload = {"username": "admin", "password": "123456"}
     r = requests.post(url, json=payload)
-    print("状态码:", r.status_code)
-    print("返回:", r.json())
+    print("状态码", r.status_code)
+    print("返回", r.json())
+
     assert r.status_code == 200
     assert r.json()["json"]["username"] == "admin"
 
-def test_with_header():
-    url = f"{BASE_URL}/get"
-    headers = {"User-Agent": "pytest-demo", "Authorization": "Bearer fake-token-123"}
-    r = requests.get(url, headers=headers)
-    assert r.status_code == 200
-    assert r.json()["headers"]["Authorization"] == "Bearer fake-token-123"
+    #Get Token
+    token = r.json()["json"]["password"]
+    hearders = {"User-Agent": "pytest-demo",
+                "Authorization": f"Bearer {token}"}
+    r = requests.get(url = f"{BASE_URL}/get", headers = hearders)
 
-def test_protected_api():
-    token = "fake-token-123"
-    headers = {"Authorization": f"Bearer {token}"}
-    r = requests.get(f"{BASE_URL}/get", headers=headers)
-    assert r.status_code == 200
-    assert r.json()["headers"]["Authorization"] == f"Bearer {token}"
+    print("GET 状态码", r.status_code)
+    print("Json打印", r.json())
